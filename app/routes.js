7d17418551ede432333c122b2f6efdffcb3f15a8
SewerynM <seweryn.majewski@kainos.com>
2021-07-08 10:45:46 +0200

    Fix viewing jobERoles on page and change file name

--- app/routes.js
+++ app/routes.js
@@ -7,8 +7,19 @@ router.get('/', (req, res) => {
     res.render('home')
 })
 
-router.get('/testjobroles', (req, res) => {
-    res.render('testjobroles')
+router.get('/jobroles', (req, res) => {
+    jobroles = [{roleID: 2, roleName: "Software Engineer", capabilityID: 3},
+                {roleID: 3, roleName: "Senior Software Engineer", capabilityID: 3},
+                {roleID: 4, roleName: "Tech Lead", capabilityID: 3},
+                {roleID: 7, roleName: "Test Engineer", capabilityID: 3},
+    ]
+
+    jobRoles = {
+            roleID: 2,
+            roleName: "Software Engineer",
+            capabilityID: 3
+    }
+    res.render('jobroles', {jobRoles: jobroles})
 })
 
 module.exports = router
