c5a6fef1d6a1e54f5f3d2baa221b6b47c0caee73
Peter Gilfedder <peter.gilfedder@kainos.com>
2021-07-08 15:07:40 +0100

    Capability column added to roles table, Role test updated to accept Strings for capability instead of int

--- src/test/java/node/SeleniumTest/DummyTest.java
+++ src/test/java/node/SeleniumTest/DummyTest.java
@@ -6,6 +6,6 @@ class DummyTest extends BaseIntegrationTest {
 
     @Test
     public void goToBrowser() {
-        driver.get("http://127.0.0.1/3000/jobRoles");
+        driver.get("www.google.co.uk");
     }
 }
