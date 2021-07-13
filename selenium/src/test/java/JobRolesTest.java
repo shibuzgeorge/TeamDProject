d23928ce69a9db9b09fccb4055ac957ebd5a72d7
Shibu George <shibu.george@kainos.com>
2021-07-13 09:42:49 +0100

    Added selenium test for get role by id

--- selenium/src/test/java/JobRolesTest.java
+++ selenium/src/test/java/JobRolesTest.java
@@ -13,11 +13,20 @@ import static org.junit.Assert.assertThat;
 public class JobRolesTest extends FunctionalTest {
 
     @Test
-    public void checkRoleNameColumnExistsOnJobRolesTable(){
+    public void getAllTableHeadings() {
         driver.get("http://localhost:3000/jobroles");
         WebElement table = driver.findElement(By.id("jobRolesTable"));
         String roleName = table.findElement(By.id("roleName")).getText();
+        String capability = table.findElement(By.id("capability")).getText();
+        String band = table.findElement(By.id("band")).getText();
+        String jobSummary = table.findElement(By.id("jobSummary")).getText();
+        String specification = table.findElement(By.id("specification")).getText();
+        String jobFamily = table.findElement(By.id("jobFamily")).getText();
 
+        assertEquals("Fail if column Capability heading doesn't exist", "Capability", capability);
+        assertEquals("Fail if column Band heading doesn't exist","Band", band);
+        assertEquals("Fail if column Job Summary heading doesn't exist", "Job Summary", jobSummary);
+        assertEquals("Fail if column Specification heading doesn't exist", "Specification", specification);
         assertEquals("Fail if column Role Name heading doesn't exist","Role Name", roleName);
     }
 
@@ -27,7 +36,7 @@ public class JobRolesTest extends FunctionalTest {
         WebElement table = driver.findElement(By.id("jobRolesTable"));
         String capability = table.findElement(By.id("capability")).getText();
 
-        assertEquals("Fail if column Capability heading doesn't exist", "Capability", capability);
+
     }
 
     @Test
@@ -69,4 +78,11 @@ public class JobRolesTest extends FunctionalTest {
         driver.findElement(By.id("band3")).click();
         assertEquals(driver.findElement(By.id("band3")).getText(), "Apprentice");
     }
+
+    @Test
+    public void getRoleById() {
+        driver.get("http://localhost:3000/role/1");
+        assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("Role: Test Engineer"));
+    }
+
 }
