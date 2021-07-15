041a1a83e04478ebeb5997371629aff2bb12c7b7
SewerynM <seweryn.majewski@kainos.com>
2021-07-15 11:15:08 +0200

    Changed routes.js and html file

--- frontend/app/routes.js
+++ frontend/app/routes.js
@@ -43,6 +43,39 @@ router.get('/role/:roleID', async (req, res) => {
   res.render('role', { role: role.data })
 })
 
+
+
+router.get('/training', async (req, res) => {
+  const bands = await axios({
+    method: 'get',
+    url: 'http://localhost:8080/api/band/getBands',
+    responseType: 'json'
+  })
+
+  res.render('training', {request: req, bands: bands.data})
+})
+
+router.get('/training/:bandID', async (req, res) => {
+  // let trainings = '';
+  // try {
+  //   trainings = await axios({
+  //     method: 'get',
+  //     url: 'http://localhost:8080/api/competency/getCompetencyByBand/' + req.params.bandID,
+  //     responseType: 'json'
+  //   })
+  // } catch (err) {
+  //   console.log('***ERROR: ', err.message)}
+
+  const bands = await axios({
+    method: 'get',
+    url: 'http://localhost:8080/api/band/getBands',
+    responseType: 'json'
+  })
+
+  // res.render('bands', {bands: bands.data, trainings: trainings.data, request: req})
+  res.render('training', {bands: bands.data, request: req})
+})
+
 router.get('/bands', async (req, res) => {
   const bands = await axios({
     method: 'get',
@@ -50,116 +83,9 @@ router.get('/bands', async (req, res) => {
     responseType: 'json'
   })
 
-    let data = [ {bands: bands.data} ];
+  let data = [ {bands: bands.data} ];
 
   res.render('bands', { data: data })
-})
-
-router.get('/training', (req, res) => {
-  var bands = [
-    {
-      bandID: 1,
-      bandName: "Apprentice"
-    },
-    {
-      bandID: 2,
-      bandName: "Trainee"
-    },
-    {
-      bandID: 3,
-      bandName: "Associate"
-    },
-    {
-      bandID: 4,
-      bandName: "Senior Associate"
-    },
-    {
-      bandID: 5,
-      bandName: "Consultant"
-    },
-    {
-      bandID: 6,
-      bandName: "Manager"
-    },
-    {
-      bandID: 7,
-      bandName: "Principal"
-    }
-  ]
-  var trainings = [
-    {
-      name: "Training 1",
-      description: "You do this and that",
-      place: "internet",
-      lowBandLevel: 1,
-      highBandLevel: 2,
-    },
-    {
-      name: "Training 2",
-      description: "You do this and that",
-      place: "internet",
-      lowBandLevel: 1,
-      highBandLevel: 1,
-    },
-    {
-      name: "Training 3",
-      description: "You do this and that",
-      place: "internet",
-      lowBandLevel: 4,
-      highBandLevel: 5,
-    },
-    {
-      name: "Training 4",
-      description: "You do this and that",
-      place: "internet",
-      lowBandLevel: 1,
-      highBandLevel: 7,
-    },
-    {
-      name: "Training 5",
-      description: "You do this and that",
-      place: "internet",
-      lowBandLevel: 5,
-      highBandLevel: 7,
-    },
-    {
-      name: "Training 6",
-      description: "You do this and that",
-      place: "internet",
-      lowBandLevel: 3,
-      highBandLevel: 7,
-    },
-    {
-      name: "Training 7",
-      description: "You do this and that",
-      place: "internet",
-      lowBandLevel: 2,
-      highBandLevel: 5,
-    },
-    {
-      name: "Training 8",
-      description: "You do this and that",
-      place: "internet",
-      lowBandLevel: 1,
-      highBandLevel: 1,
-    },
-    {
-      name: "Training 9",
-      description: "You do this and that",
-      place: "internet",
-      lowBandLevel: 4,
-      highBandLevel: 4,
-    },
-    {
-      name: "Training 10",
-      description: "You do this and that",
-      place: "internet",
-      lowBandLevel: 5,
-      highBandLevel: 5,
-    }
-  ]
-
-  res.render('training', {request: req, bands: bands, trainings: trainings})
 })
 
 
--- frontend/public/javascripts/training.js
+++ frontend/public/javascripts/training.js
@@ -1,8 +1,8 @@
-window.onload = function(){
-    const btn = document.querySelector("#band")
-    const form = document.querySelector("#bandChoice")
-
-    btn.onchange = function(){
-        form.submit()
-    }
-}
+// window.onload = function(){
+//     const btn = document.querySelector("#band")
+//     const form = document.querySelector("#bandChoice")
+//
+//     btn.onchange = function(){
+//         form.submit()
+//     }
+// }
