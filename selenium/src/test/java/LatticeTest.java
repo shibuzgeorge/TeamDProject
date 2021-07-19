b6b83205b2bcf59b4d14774de862f7e54b159bcc
Peter Gilfedder <peter.gilfedder@kainos.com>
2021-07-19 14:51:24 +0100

    Career lattice layout updated CSS file added lattice test added

---
+++ selenium/src/test/java/LatticeTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import framework.FunctionalTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.Assert.assertEquals;
+
+public class LatticeTest extends FunctionalTest {
+}
