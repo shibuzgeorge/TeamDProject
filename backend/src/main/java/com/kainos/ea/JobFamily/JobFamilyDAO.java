fd86bffb0b2cdff6a72e7bb4957e169e77dd7742
Shibu George <shibu.george@kainos.com>
2021-07-22 09:13:57 +0100

    Added backend for edit job family

--- backend/src/main/java/com/kainos/ea/JobFamily/JobFamilyDAO.java
+++ backend/src/main/java/com/kainos/ea/JobFamily/JobFamilyDAO.java
@@ -17,10 +17,18 @@ public interface JobFamilyDAO {
     @SqlQuery("SELECT * FROM JobFamilyFull R WHERE R.capabilityName = :capability;")
     List<JobFamily> getJobFamilyByCapability(@Bind("capability") String capability);
 
+    @SqlQuery("SELECT * FROM JobFamilyFull R WHERE R.jobFamilyID = :id;")
+    JobFamily getJobFamilyByID(@Bind("id") int id);
+
     @SqlUpdate("INSERT INTO JobFamily (capabilityID, jobFamilyName) VALUES ((SELECT capabilityID FROM Capability WHERE capabilityName = :capability), :jobFamilyName);")
     boolean insertNewJobFamily(@Bind("jobFamilyName") String jobFamilyName, @Bind("capability") String capability);
 
     @SqlQuery("SELECT * FROM JobFamilyFull WHERE jobFamilyName = :jobFamilyName AND capabilityName = :capability;")
     JobFamily checkIfJobFamilyNameAndCapabilityComboExist(@Bind("jobFamilyName") String jobFamilyName, @Bind("capability") String capability);
 
+    @SqlUpdate("UPDATE JobFamily SET jobFamilyName = :jobFamilyName WHERE jobFamilyID = :id;")
+    boolean editJobFamilyName(@Bind("jobFamilyName") String jobFamilyName, @Bind("id") int id);
+
+    @SqlUpdate("UPDATE JobFamily SET capabilityID = (SELECT capabilityID FROM Capability WHERE capabilityName = :capability) WHERE jobFamilyID = :id;")
+    boolean editJobFamilyCapability(@Bind("capability") String capability, @Bind("id") int id);
 }
